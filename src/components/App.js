a9f888cb49de1e14f6908b1f2321d59b1e59f6ed
jameschristou <james.christou@gmail.com>
2019-07-06 08:15:57 +1000

    Moving currentProcessor state to the app level and introducing processorSelectedHandler at the app level

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProcessorSelectorComponent from './ProcessorSelectorComponent';
 import StatusComponent from './StatusComponent';
 import StartStopComponent from './StartStopComponent';
@@ -11,10 +11,25 @@ const configValue = {
 };
 
 const AppComponent = (props) => {
+  const [currentProcessor, setProcessor] = useState('');
+
+  /* #region Main */
+  const processorSelectedHandler = (e, favoriteValue) => {
+    e.preventDefault();
+    // const sessionId = parseInt(e.target.attributes["data-sessionid"].value);
+    // dispatch({
+    //   type: favoriteValue === true ? "favorite" : "unfavorite",
+    //   sessionId
+    // });
+    console.log('Processor changed');
+  };
+
+  /* #endregion */
+
   return (
     <div className="posts-processor-app">
       <ConfigContext.Provider value={configValue} >
-        <ProcessorSelectorComponent />
+        <ProcessorSelectorComponent processorSelectedHandler={processorSelectedHandler}/>
         <StatusComponent />
         <StartStopComponent />
         <ProcessedPostsComponent />
--- src/components/ProcessorSelectorComponent.js
+++ src/components/ProcessorSelectorComponent.js
@@ -1,24 +1,9 @@
-import React, {useState, useEffect, useReducer, useContext } from 'react';
+import React, {useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { ConfigContext } from "./App";
 
-const processorSelectedReducer = (state, action) => {
-  switch (action) {
-    case 'PROCESSOR_SELECTED':
-      console.log('PROCESSOR_SELECTED action');
-      return 'PROCESSOR_IS_SELECTED';
-    case 'NO_PROCESSOR_SELECTED':
-      console.log('NO_PROCESSOR_SELECTED action');
-      return 'PROCESSOR_NOT_SELECTED';
-    default:
-      throw new Error();
-  }
-};
-
-const ProcessorSelectorComponent = (props) => {
-  // use array destructuring to get the getState function (we don't need a function to set state since we only do that once on component load)
-  const [state, setState] = useState([{'value': 'default', 'name': 'Choose a Processor'}]);
-  const [processorSelectedState, dispatchProcessorSelected] = useReducer(processorSelectedReducer, 'NO_PROCESSOR_SELECTED');
+const ProcessorSelectorComponent = ({processorSelectedHandler}) => {
+  const [processorListOptions, setProcessorListOptions] = useState([{'value': 'default', 'name': 'Choose a Processor'}]);
 
   const context = useContext(ConfigContext);
 
@@ -30,32 +15,23 @@ const ProcessorSelectorComponent = (props) => {
       );
       
       // clone the current state (initial state)...state is immutable so can't modify it directly
-      let options = [...state];
+      let options = [...processorListOptions];
 
       result.data.processors.forEach(processor => {
         options.push({value: processor, name: processor});
       })
 
-      setState(options);
+      setProcessorListOptions(options);
     };
 
     fetchData();
   }, []);
 
-  const handleProcessorSelection = () => {
-    if(true){
-      dispatchProcessorSelected('PROCESSOR_SELECTED');
-      return;
-    }
-
-    dispatchProcessorSelected('NO_PROCESSOR_SELECTED');
-  };
-
   return (
     <div className="panel file-info">
         <label>Processor</label>
-        <select id="processor-name" type="text" name="processor-name" onChange={handleProcessorSelection}>
-          {state.map((option) =>
+        <select id="processor-name" type="text" name="processor-name" onChange={processorSelectedHandler}>
+          {processorListOptions.map((option) =>
               <option key={option.value}>{option.name}</option>
           )}
         </select>
