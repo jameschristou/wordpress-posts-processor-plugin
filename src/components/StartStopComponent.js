3e790c393a67e0bbe19c04477eea7acdce174c13
jameschristou <james.christou@gmail.com>
2019-11-10 21:32:38 +1100

    Adding in loading gif to indicate processor is running

--- src/components/StartStopComponent.js
+++ src/components/StartStopComponent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const StartStopComponent = ({isEnabled, isProcessing, startStopProcessingHandler}) => {
+
+
   return (
     <div className="start-stop">
         <input id="start-process" className={'start-stop__start button button-primary' + (isProcessing || !isEnabled ? ' disabled' : '')} type="submit" name="start-process" value="Start" onClick={event => startStopProcessingHandler('start')}/>
         <input id="stop-process" className={'button button-primary' + (!isProcessing || !isEnabled ? ' disabled' : '')} type="submit" name="stop-process" value="Stop" onClick={event => startStopProcessingHandler('stop')}/>
+        <span className={`spinner ${isProcessing ? "is-active" : ""}`}></span>
     </div>
   );
 }
